973b24d42c8fb5a58ff89b08f70d4149723d1d62
Coolafdood <chazeldred55@gmail.com>
2026-01-25 20:23:43 +0000

    Add shopping bag view and resolve URL import error

--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from products.models import Product
+
 
 def view_bag(request):
-    """ A view to render the shopping bag contents page """
+    """ Display the contents of the shopping bag """
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, item_id):
+    """ Add a quantity of the specified product to the shopping bag """
+
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+
+    bag = request.session.get('bag', {})
+
+    if item_id in bag:
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+
+    request.session['bag'] = bag
+    return redirect(redirect_url)
