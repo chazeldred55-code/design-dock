54c005307e96fe3d67844c2df5fdc4c67ca547ad
Coolafdood <chazeldred55@gmail.com>
2026-02-04 07:52:19 +0000

    Add country dropdown with django-countries and improve checkout UX

--- checkout/models.py
+++ checkout/models.py
@@ -1,8 +1,9 @@
 import uuid
 
+from django.conf import settings
 from django.db import models
 from django.db.models import Sum
-from django.conf import settings
+from django_countries.fields import CountryField
 
 from products.models import Product
 
@@ -12,7 +13,10 @@ class Order(models.Model):
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
-    country = models.CharField(max_length=40, null=False, blank=False)
+
+    # Country uses ISO 3166-1 alpha-2 codes via django-countries (Stripe-compatible)
+    country = CountryField(blank_label="Country *", null=False, blank=False)
+
     postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
@@ -28,9 +32,24 @@ class Order(models.Model):
 
     date = models.DateTimeField(auto_now_add=True)
 
-    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
-    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
-    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    delivery_cost = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        null=False,
+        default=0
+    )
+    order_total = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        null=False,
+        default=0
+    )
+    grand_total = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        null=False,
+        default=0
+    )
 
     def _generate_order_number(self):
         """
@@ -43,13 +62,17 @@ class Order(models.Model):
         Update grand total each time a line item is added,
         accounting for delivery costs.
         """
-        self.order_total = self.lineitems.aggregate(
-            Sum("lineitem_total")
-        )["lineitem_total__sum"] or 0
+        self.order_total = (
+            self.lineitems.aggregate(Sum("lineitem_total"))[
+                "lineitem_total__sum"
+            ] or 0
+        )
 
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = (
-                self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+                self.order_total
+                * settings.STANDARD_DELIVERY_PERCENTAGE
+                / 100
             )
         else:
             self.delivery_cost = 0
@@ -82,10 +105,18 @@ class OrderLineItem(models.Model):
         Product,
         null=False,
         blank=False,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
     )
-    product_size = models.CharField(max_length=2, null=True, blank=True)
-    quantity = models.IntegerField(null=False, blank=False, default=0)
+    product_size = models.CharField(
+        max_length=2,
+        null=True,
+        blank=True
+    )
+    quantity = models.IntegerField(
+        null=False,
+        blank=False,
+        default=0
+    )
     lineitem_total = models.DecimalField(
         max_digits=6,
         decimal_places=2,
