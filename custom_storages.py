3c90336030b55da1e103ad8ccbdb1d6c44ba1d2a
Coolafdood <chazeldred55@gmail.com>
2026-02-18 14:56:54 +0000

    Disable S3 ACLs for static/media storage

--- custom_storages.py
+++ custom_storages.py
@@ -1,13 +1,14 @@
+from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticStorage(S3Boto3Storage):
-    location = "static"
-    default_acl = "public-read"
+    location = getattr(settings, "STATICFILES_LOCATION", "static")
+    default_acl = None          # ✅ bucket has ACLs disabled
     file_overwrite = True
 
 
 class MediaStorage(S3Boto3Storage):
-    location = "media"
-    default_acl = "public-read"
+    location = getattr(settings, "MEDIAFILES_LOCATION", "media")
+    default_acl = None          # ✅ bucket has ACLs disabled
     file_overwrite = False
